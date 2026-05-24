42dd4c48096d1e79746c73ba3eb91e76169ddeba
abdulloherkinov667-create <abdulloherkinov667@gmail.com>
2026-05-24 16:27:43 +0500

    .

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def error_html(request):
-    return render(request, 'error.html')
+    return render(request, 'kirish.html')
 
 def home_run_page(request):
     return render(request, 'kirish.html')
